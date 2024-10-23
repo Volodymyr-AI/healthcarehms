d067909a43e701d469e0f9a2a301c418d5423fe5
Volodymyr-AI <alternativeqoob@gmail.com>
2024-10-23 11:43:09 +0300

    Deleted unnecessary backend sln

---
+++ backend/src/HealthcareHMS/HospitalManagement.Application/Doctors/Commands/CreateDoctorInfo/CreateDoctorInfoCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace HospitalManagement.Application.Doctors.Commands.CreateDoctorInfo
+{
+    public class CreateDoctorInfoCommand : IRequest<Guid>
+    {
+        public Guid HospitalAdminId { get; set; }
+        
+    }
+}
