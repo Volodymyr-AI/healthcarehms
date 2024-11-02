6cacbcc60ed63847f81901b780c459e0ddd6fdb2
Volodymyr-AI <alternativeqoob@gmail.com>
2024-11-02 21:01:48 +0200

    Removed StaffMS as it currently in development status

--- backend/Services/HelthcareHMS/StaffMS.WebAPI/Domain/UserRelated/GlobalAdminEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StaffMS.WebAPI.Domain.UserRelated;
-
-public class GlobalAdminEntity
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Phone { get; set; }
-}
--- backend/Services/HelthcareHMS/UserManagement.Service/Program.cs
+++ backend/Services/HelthcareHMS/UserManagement.Service/Program.cs
@@ -49,6 +49,18 @@ namespace UserManagement.Service
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             
+            //CORS  
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+            
             builder.Services.AddScoped<GlobalAdminService>();
             builder.Services.AddScoped<StaffAdminService>();
             builder.Services.AddScoped<StaffMemberService>();
@@ -67,6 +79,8 @@ namespace UserManagement.Service
 
             app.UseHttpsRedirection();
 
+            app.UseCors("AllowAll");
+            
             app.UseAuthentication();
             app.UseAuthorization();
 
